a00cf6d98de3070efdc2e8093ed0b00125030c10
whoami847 <mrbeastbangla069@gmail.com>
2025-05-05 11:34:24 +0600

    Update utils.py

--- utils.py
+++ utils.py
@@ -10,24 +10,20 @@ import math
 
 logger = logging.getLogger(__name__)
 
-
 async def progress(done, total, message, start_time):
     """Display progress while downloading."""
-
     present = time.time()
     percentage = done * 100 / total
     progressbar = f"[{'▪️' * math.floor(percentage/10)}{'▫️' * (10 - math.floor(percentage/10))}]"
-    text = progressbar + f"\n{human_redable(done)} of {human_redable(total)}"
+    text = progressbar + f"\n{human_readable(done)} of {human_readable(total)}"
     if round((present - start_time) % 3) == 0 or done == total:
         try:
             await message.edit(text)
         except Exception as e: # pylint: disable=broad-exception-caught
             logger.warning(e)
 
-
-def human_redable(size):
+def human_readable(size):
     """Convert bytes to human-readable format."""
-
     size_units = ["B", "KB", "MB", "GB", "TB"]
     for unit in size_units:
         formatted_size = f"{round(size, 2)} {unit}"
